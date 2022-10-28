6fd7bacaf0b209abe2241c63517ec3433d82aebc
Saritik <saritik16@gmail.com>
2022-10-28 13:23:55 +0300

    Initial code and Welcome function

---
+++ Stage0/Program0048.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void main(string[] srgs)
+        {
+            Welcome0048();
+            Welcome2942();
+            Console.ReadKey();
+        }
+        private static void Welcome0048()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome2942();
+    }
+}
