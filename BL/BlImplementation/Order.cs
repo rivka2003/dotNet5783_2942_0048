411a0926fb96dd598a67d503d95cf94da3b1881f
rivka2003 <rivkablum7@gmail.com>
2022-11-23 21:04:54 +0200

    order changes

--- BL/BlImplementation/Order.cs
+++ BL/BlImplementation/Order.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using BlApi;
 using BO;
+using CopyPropertisTo;
 using Dal;
 using DalApi;
+using DO;
 
 namespace BlImplementation
 {
@@ -20,25 +22,89 @@ namespace BlImplementation
             IEnumerable<BO.OrderForList> newListOfOrders = listOfOrders.Select(item => (OrderForList)item = );
         }
 
-        public BO.OrderForList OrderDetails(int ID)
+        /// <summary>
+        /// trying.........
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="NotImplementedException"></exception>
+        public BO.Order OrderDetails(int ID)
         {
-            return null;
+            BO.Order OrderBo = new BO.Order();
+            DO.Order OrderDo = new DO.Order();
+            if (ID > 0)
+            {
+                try 
+                { 
+                      OrderDo = Dal.Order.RequestById(ID); 
+                }
+                catch (DO.NonFoundObject)
+                { throw new Exception("not found"); }
+            
+            {
+                OrderDo.CopyPropTo(OrderBo);
+            }
+            
+            return OrderBo;
         }
-
+             
         public BO.OrderTracking TrackingOrder(int ID)
         {
-            return null;
+                BO.Order OrderBo = new BO.Order();
+                DO.Order OrderDo = new DO.Order();
+                try
+                    {
+                        OrderDo = Dal.Order.RequestById(ID);
+                    }
+                catch (DO.NonFoundObject)
+                    { throw new Exception("not found"); }
+
+                if(OrderDo.ShipDate<DateTime.Now())///if it didnt pass)
+                {
+                    OrderDo.///updates
+                        ///also update logical layer
+
+                }
+
+                    
+
+                
+                return OrderBo;
         }
 
         public BO.Order UpdateDeliveryDate(int ID)
         {
-            return null;
-        }
 
-        public BO.Order UpdeteShipDate(int ID)
+                BO.Order OrderBo = new BO.Order();
+                DO.Order OrderDo = new DO.Order();
+                try
+                {
+                    OrderDo = Dal.Order.RequestById(ID);
+                }
+                catch (DO.NonFoundObject)
+                { throw new Exception("not found"); }
+                if(OrderDo.ShipDate///passed but OrderDo.status==0)
+                        {
+                    OrderDo.ShipDate///update;
+                    ///update in the logical layer as well
+                        }
+                return OrderBo;
+
+            }
+
+            public BO.Order UpdeteShipDate(int ID)
         {
-            return null;
-        }
+                BO.Order OrderBo = new BO.Order();
+                DO.Order OrderDo = new DO.Order();
+                try
+                {
+                    OrderDo = Dal.Order.RequestById(ID);
+                }
+                catch (DO.NonFoundObject)
+                { throw new Exception("not found"); }
+                return OrderBo;
+            }
 
         IEnumerable<OrderForList> BlApi.IOrder.AllList(List<DO.Order> listOfOrders)
         {
