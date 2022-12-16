a3e720cda6b6b6e1d96fdf197befe47aefb03fe7
Saritik <saritik16@gmail.com>
2022-12-16 12:24:16 +0200

    6.5

---
+++ DalXml/XmlDataSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class XmlDataSource
+    {
+        static XmlDataSource()
+        {
+            s_Initialize();
+        }
+
+        private static void s_Initialize()
+        {
+            InitProduct();
+            InitOrder();
+            InitOrderItem();
+        }
+    }
+}
